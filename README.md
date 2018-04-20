9c49a3fd23edfc150dd8858d243d06ff16fd46f6
john-b-yang <byjohnyang@gmail.com>
2018-04-19 23:24:55 -0700

    observations

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # BETS Driver Testing Framework
 Universal testing framework for BETS driver code
+
+### Driver Commonalities:
+#### main.go
+- Creates 'service' from bwClient.RegisterService() function
+- Register each device as independent interface using service.RegisterInterface() function
+- Ponum for creating and decoding message structs
+
+#### "driver".go
--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	bw2 "gopkg.in/immesys/bw2bind.v5"
+)
+
+/*
+Parameters:
+- clientName: Name of thermostat for creating new service client (i.e. s.pelican, s.national-weather-service)
+*/
 
 func main() {
+
+}
+
+func test(clientName string, interfaceName string) bool {
+	// Part 1: Establish Bosswave Connection
 	fmt.Println("Hello World")
+	bwClient := bw2.ConnectOrExit("")
+	bwClient.OverrideAutoChainTo(true)
+	bwClient.SetEntityFromEnvironOrExit()
+
+	// Part 2: Create Service & Interface Clients
+	baseURI := "john/test/"
+	serviceClient := bwClient.NewServiceClient(baseURI, clientName)
+
+	// Part 3: Create Payload Packages
+
+	// Part 4: Send Payload Packages and Parse Outputs
+
+	// Part 5: Use user function to check whether output is correct
+	// Return "True" to indicate test was passed, "False" for visa versa
+	return true
 }
